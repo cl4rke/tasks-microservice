b9cf98b9c45ff177bfc22988237f3ab00c7b7808
Clarke Benedict Plumo <clarke.plumo@gmail.com>
2015-12-10 14:49:28 +0800

    Add api models

---
+++ api/models.py
@@ -0,0 +1,38 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class SkillValue(models.Model):
+    user = models.ForeignKey(User)
+    skill = models.ForeignKey(Skill)
+    value = models.IntegerField()
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    assigned_to = models.ForeignKey(User, related_name='assigned_user_id')
+    estimated_time = models.IntegerField()
+    time_completed = models.IntegerField()
+    is_completed = models.BooleanField()
+    dependency = models.ForeignKey('Task', blank=True, null=True)
+    created_by = models.ForeignKey(User, related_name='created_by_user_id')
+
+
+class Absence(models.Model):
+    date = models.DateField()
+    is_approved = models.BooleanField()
+    user = models.ForeignKey(User)
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, related_name='sender_user_id')
+    receiver = models.ForeignKey(User, related_name='receiver_user_id')
+    name = models.TextField()
+
