4620bf27befe45798ee0047ce14ccfed6b36629e
Clarke Benedict Plumo <clarke.plumo@gmail.com>
2015-12-15 14:06:14 +0800

    Add created_at and updated_at to admin displayed list

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.apps import apps
 
+class RecordedModelAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'created_at', 'updated_at')
+
+
 app = apps.get_app_config('api')
 
 for model_name, model in app.models.items():
-    admin.site.register(model)
+    admin.site.register(model, RecordedModelAdmin)
 
